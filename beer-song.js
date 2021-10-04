648f434b7d97692c67a539525e259190dfec1086
David Meribe <davidmeribe.official@gmail.com>
2021-10-04 11:51:32 -0400

    Final Solution

--- beer-song.js
+++ beer-song.js
@@ -4,38 +4,41 @@ class BeerSong {
       this.endVerse = 0;
     } 
 
-  sing(start, end) {
-    let song = '';
-    if (!start && !end) {
-       // sing all verses
-          start = 99;
-          end = 0;
-    } else if (start && !end) {
-            // sing from start to 0
-             end = 0;
-    }
+  static sing(start = 99, end =0) {
+    let song = [];
     // else start & end is already set, continue song logic
     for (let i = start; i >= end; i--) {
-         song += this.buildVerse(i);
+        song.push(this.verse(i));
     }
-    return song;
+    //console.log(song);
+    return song.join('\n');
   }
 
-  verse(verseNum) {
-    return this.buildVerse(verseNum);
+  static verse(verseNum) {
+    if (verseNum == 0){
+        return ('No more bottles of beer on the wall, no more bottles of beer.\n' +
+               'Go to the store and buy some more, 99 bottles of beer on the wall.\n');
+    }else if (verseNum == 1){
+        return ('1 bottle of beer on the wall, 1 bottle of beer.\n' +
+     'Take it down and pass it around, no more bottles of beer on the wall.\n');
+    }
+    else if (verseNum == 2){
+     return ('2 bottles of beer on the wall, 2 bottles of beer.\n' +
+     'Take one down and pass it around, 1 bottle of beer on the wall.\n');
+    }
+    else{
+        return (
+         verseNum + ' bottles of beer on the wall, ' + verseNum + ' bottles of beer.\n'
+         + 'Take one down and pass it around, ' + (verseNum - 1) + ' bottles of beer on the wall.\n'
+        );
+    }
   }
 
-  buildVerse(verseNum) {
-   if (verseNum == 0){
-       return ('No more bottles of beer on the wall, no more bottles of beer.\n' +
-              'Go to the store and buy some more, 99 bottles of beer on the wall. \n');
-   }else{
-       return (
-        verseNum + ' bottles of beer on the wall,' + verseNum + ' bottles of beer.\n'
-        + 'Take one down and pass it around,' + (verseNum - 1) + ' bottles of beer on the wall.\n'
-       );
-   }
-  }
 }
 
 export default BeerSong;
+
+// for (let i = start; i >= end; i--) {
+//   song += this.verse(i);
+//   //console.log(this.buildVerse(i));
+// }
